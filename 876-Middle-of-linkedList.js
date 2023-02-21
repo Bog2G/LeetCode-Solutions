0bd87aefe456f0cd43115a426c97854e6ea60be4
Melfric <wegweg3eg@abv.bg>
2023-02-21 22:46:09 +0200

    Added 557 and 876

---
+++ 876-Middle-of-linkedList.js
@@ -0,0 +1,22 @@
+var middleNode = function(head) {
+    let current = head
+    let count = 0
+    while (current){
+        current = current.next
+        count++;
+    
+    }
+    console.log(count)
+    current = head
+    if (count % 2 == 0){
+        for (let i = 0; i < count/2; i++){
+            current = current.next
+        }
+        return current
+    } else {
+        for (let i = 0; i < count/2 - 1; i++){
+            current = current.next
+        }
+        return current
+    }
+};

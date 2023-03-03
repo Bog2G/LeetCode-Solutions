c33cbe36519af6c3239aa53095b6e04f4a378251
Melfric <wegweg3eg@abv.bg>
2023-03-03 23:36:38 +0200

    Added 48

---
+++ 48-Rotate-Image.js
@@ -0,0 +1,10 @@
+var rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i; j < matrix.length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+    matrix[i].reverse();
+  }
+
+  return matrix;
+};
